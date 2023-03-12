bfa2ffd56a49575c6396370052581e9498a74729
sam <lingourui@gmail.com>
2023-03-13 01:57:31 +0800

    fix#3 add validation and try catch in WordController

--- src/main/java/com/sam/wordkeeperj/controller/WordController.java
+++ src/main/java/com/sam/wordkeeperj/controller/WordController.java
@@ -8,6 +8,10 @@ import com.sam.wordkeeperj.entity.Word;
 import com.sam.wordkeeperj.service.MeaningService;
 import com.sam.wordkeeperj.service.SentenceService;
 import com.sam.wordkeeperj.service.WordService;
+import org.apache.ibatis.javassist.tools.web.BadHttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 
@@ -30,29 +34,42 @@ public class WordController {
     }
 
     @GetMapping("/api/words/{wordName}")
-    public Word findWordByWordName(@PathVariable("wordName") String wordName) {
-        return this.wordService.find(wordName);
+    public ResponseEntity<Word> findWordByWordName(@PathVariable("wordName") String wordName) {
+        Word word = this.wordService.find(wordName);
+        return new ResponseEntity<Word>(word, HttpStatus.OK);
     }
 
     @GetMapping("/api/categories")
-    public List<Map<String, String>> getAllCategories() {
-        return this.wordService.getAllCategories();
+    public ResponseEntity<List<Map<String, String>>> getAllCategories() {
+        List<Map<String, String>> ret = this.wordService.getAllCategories();
+        return new ResponseEntity<>(ret, HttpStatus.OK);
     }
 
     @GetMapping("/api/categories/{category}")
-    public String getCategoryChapterInfo(@PathVariable String category) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        List<Map<String, String>> categoryChapterInfo = this.wordService.getCategoryChapterInfo(category);
-        return mapper.writeValueAsString(categoryChapterInfo);
+    public ResponseEntity<String> getCategoryChapterInfo(@PathVariable String category) throws JsonProcessingException {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            List<Map<String, String>> categoryChapterInfo = this.wordService.getCategoryChapterInfo(category);
+            String ret = mapper.writeValueAsString(categoryChapterInfo);
+            return new ResponseEntity<>(ret, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/api/words/{category}/{chapter}")
-    public List<Word> getChapterWords(@PathVariable String category, @PathVariable String chapter) {
-        return this.wordService.getChapterWords(category, chapter);
+    public ResponseEntity<List<Word>> getChapterWords(@PathVariable String category, @PathVariable String chapter) {
+        List<Word> words = this.wordService.getChapterWords(category, chapter);
+        return new ResponseEntity<>(words, HttpStatus.OK);
     }
 
     @PutMapping("/api/words/{wordName}")
-    public void updateWord(@PathVariable("wordName") String wordName, @RequestBody Word word) {
+    @Transactional
+    public ResponseEntity<HttpStatus> updateWord(@PathVariable("wordName") String wordName, @RequestBody Word word) {
+        if (!wordName.equals(word.getWordName())) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        Word tmp = this.wordService.find(wordName);
+        if (tmp == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         this.wordService.updateWord(word);
 
         List<Meaning> meanings = word.getMeanings();
@@ -76,10 +93,16 @@ public class WordController {
                             this.sentenceService.updateSentence(sentence);
                     });
         }
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/api/words")
-    public void addWord(@RequestBody Word word) {
+    @Transactional
+    public ResponseEntity<HttpStatus> addWord(@RequestBody Word word) {
+        Word tmp = this.wordService.find(word.getWordName());
+        if (tmp != null) throw new RuntimeException("the word:" + word.getWordName() + "has been existed");
+
         this.wordService.addWord(word);
 
         List<Meaning> meanings = word.getMeanings();
@@ -95,5 +118,6 @@ public class WordController {
                 this.sentenceService.addSentence(sentence);
             });
         }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
